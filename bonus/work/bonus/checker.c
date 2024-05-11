d1ef625803cd0e5e07a4c3779588f584699e20dd
Laabdi <119082919+Laabdi@users.noreply.github.com>
2024-05-11 15:46:10 +0100

    d

---
+++ bonus/work/bonus/checker.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moaregra <moaregra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 12:09:18 by moaregra          #+#    #+#             */
+/*   Updated: 2024/05/09 15:30:30 by moaregra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+static void	free_str_array(char **arr)
+{
+	int	i;
+
+	if (arr)
+	{
+		i = 0;
+		while (arr[i] != NULL)
+		{
+			free(arr[i]);
+			i++;
+		}
+		free(arr);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+	int		stack_size;
+	char	**nums;
+
+	if (ac < 2)
+		return (0);
+	nums = splited(av);
+	if (!nums)
+		return (1);
+	if (!check_valid(nums))
+	{
+		free_str_array(nums);
+		exit_error(NULL, NULL);
+	}
+	stack_b = NULL;
+	stack_a = fill_stack(nums);
+	stack_size = get_stack_size(stack_a);
+	assign_index(stack_a, stack_size + 1);
+	ft_loop(&stack_a,&stack_b);
+	push_swap(&stack_a, &stack_b, stack_size);
+    if(ft_is_sorted(&stack_a) == 1 && stack_b == NULL)
+    	OK_handler();
+    	else
+        KO_handler();
+	free_stack(stack_a);
+	if (stack_b != NULL)
+		free(stack_b);
+	free_str_array(nums);
+	return (0);
+}
+
---
+++ bonus/work/bonus/reading.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reading.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moaregra <moaregra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 12:09:20 by moaregra          #+#    #+#             */
+/*   Updated: 2024/05/09 16:54:49 by moaregra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int ft_strncmp(char *s1,char *s2,int n)
+{
+	int  i = 0;
+	while(i < n)
+	{
+		if(s1[i] == s2[i])
+		i++;
+	}
+	return(s1[i] - s2[i]);
+}
+
+void read_execution(t_list **stack_a,t_list **stack_b,char *str)
+{
+	if(ft_strncmp(str,"pb\n",3))
+		do_pb_bonus(stack_a,stack_b);
+	else if(ft_strncmp(str,"pa\n",3))
+		do_pa_bonus(stack_a,stack_b);
+	else if(ft_strncmp(str,"rb\n",3))
+		do_rb_bonus(stack_b);
+	else if(ft_strncmp(str,"ra\n",3))
+		do_ra_bonus(stack_a);
+	else if(ft_strncmp(str,"rr\n",3))
+		do_rr_bonus(stack_a,stack_b);
+	else if(ft_strncmp(str,"rra\n",4))
+		do_rra_bonus(stack_a);
+	else if(ft_strncmp(str,"rra\n",4))
+		do_rra_bonus(stack_a);
+	else if(ft_strncmp(str,"rrr\n",4))
+		do_rrr_bonus(stack_a, stack_b);
+	else if(ft_strncmp(str,"sa\n",3))
+		do_sa_bonus(stack_a);
+	else if(ft_strncmp(str,"sb\n",3))
+		do_sb_bonus(stack_b);
+	else if(ft_strncmp(str,"sa\n",3))
+		do_sa_bonus(stack_a);
+}
+int	check_in(char *str)
+{
+	int	flag;
+
+	flag = 0;
+	if (ft_strncmp(str, "pb\n", 3) == 0
+		|| ft_strncmp(str, "pa\n", 3) == 0)
+		flag = 1;
+	else if (ft_strncmp(str, "ra\n", 3) == 0
+		|| ft_strncmp(str, "rb\n", 3) == 0)
+		flag = 1;
+	else if (ft_strncmp(str, "rr\n", 3) == 0)
+		flag = 1;
+	else if (ft_strncmp(str, "rra\n", 4) == 0
+		|| ft_strncmp(str, "rrb\n", 4) == 0)
+		flag = 1;
+	else if (ft_strncmp(str, "rrr\n", 4) == 0)
+		flag = 1;
+	else if (ft_strncmp(str, "sa\n", 3) == 0)
+		flag = 1;
+	else if (ft_strncmp(str, "sb\n", 3) == 0)
+		flag = 1;
+	else if (ft_strncmp(str, "ss\n", 3) == 0)
+		flag = 1;
+	return (flag);
+}
